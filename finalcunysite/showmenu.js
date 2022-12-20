0a7480351614dd23d74a60f35830f74c51106974
Tasia Miller <112511590+tasiamiller@users.noreply.github.com>
2022-12-19 23:13:00 -0500

    Add files via upload

---
+++ finalcunysite/showmenu.js
@@ -0,0 +1,8 @@
+  function showMenu(){
+        var  menu = document.getElementById("dropdown-menu");
+        if  (menu.style.display==="none"){
+            menu.style.display="block";
+        } else {
+        menu.style.display="none";
+            }
+        }
